90291e47f118119da2c5b6cd612ad2ca316755fc
Arne Kristian Drangsholt <arnekristian@hotmail.com>
2019-05-12 18:58:02 +0200

    Fikset unicode

--- unicode/unicode.go
+++ unicode/unicode.go
@@ -12,11 +12,12 @@ func Translate(expression string, language string) string {
 	if expression == "nord og sør" {
 		if language == "jp" {
 			uni = "\xe2\x80\x9c" + expression + "\xe2\x80\x9d" + " på japansk: " +
-				"\xe2\x80\x9c\xe5\x8c\x97\xe3\x81\xa8\xe5\x8d\x97\xe2\x80\x9d" //e2 80 9c e5 8c 97 e3 81 a8 e5 8d 97 e2 80 9d
+				"\x22\xe5\x8c\x97\xe3\x81\xa8\xe5\x8d\x97\x22"
+			//22 E5 8C 97 E3 81 A8 E5 8D 97 22
 		} else if language == "is" {
 			uni = "\xe2\x80\x9c" + expression + "\xe2\x80\x9d" + " på islandsk: " +
-				"\xe2\x80\x9c\x6e\x6f\x72\xc3\xb0\x75\x72\x20\x6f\x67\x20\x73\x75\xc3\xb0\x75\x72\xe2\x80\x9d"
-			//e2 80 9c 6e 6f 72 c3 b0 75 72 20 6f 67 20 73 75 c3 b0 75 72 e2 80 9d
+				"\x22\x6E\x6F\x72\xC3\xB0\x75\x72\x20\x6F\x67\x20\x73\x75\xC3\xB0\x75\x72\x22"
+			//22 6E 6F 72 C3 B0 75 72 20 6F 67 20 73 75 C3 B0 75 72 22
 		}
 	}
 	return uni
--- unicode_main.go
+++ unicode_main.go
@@ -7,11 +7,19 @@ import (
 
 func main() {
 
+	const islandsk = "\"norður og suður\""
+	const japansk = "\"北と南\""
+
+	fmt.Printf("\"norður og suður\" til heksadesimal: % X\n", islandsk)
+	fmt.Printf("\"北と南\" til heksadesimal: % X\n", japansk)
+	fmt.Println()
+
 	jp := unicode.Translate("nord og sør", "jp")
 	is := unicode.Translate("nord og sør", "is")
 
 	fmt.Println(jp)
 	fmt.Println(is)
+	fmt.Println()
 
 	unicode.UnicodeCodePointDemo()
 
