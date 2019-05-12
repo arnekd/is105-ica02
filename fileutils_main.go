de55d64c086bd274339f43b03d7cfd1de34647ce
Arne Kristian Drangsholt <arnekristian@hotmail.com>
2019-05-12 14:05:35 +0200

    endret på fileutils, ikke ferdig

--- fileutils_main.go
+++ fileutils_main.go
@@ -11,9 +11,20 @@ func main() {
 	text2 := fileutils.FileToByteslice("files/lang02.wl")
 	text3 := fileutils.FileToByteslice("files/lang03.wl")
 
-	fmt.Printf("lang01:\n% X\n", text1)
-	fmt.Printf("lang02:\n% X\n", text2)
-	fmt.Printf("lang03:\n% X\n", text3)
+	fmt.Printf("lang01.wl:\n% c\n", text1)
+	fmt.Printf("lang02.wl:\n% c\n", text2)
+	fmt.Printf("lang03.wl:\n% c\n", text3)
+
+	fmt.Println()
+
+	//text1first16 := "\xEF\xDA\xA3\xD2\xD3\xCB\x0A\xEF\xDA\xA3\xD2\xD3\xCB\xC1\x0A\xEF"
+	//fmt.Printf("%c\n", byte[]("\xEF"))
+	fmt.Printf("%s", "\xc3\xaf\xc3\x9a")
+
+	//1110 1111 --> 11000011 10101111 --> C3 AF
+	//1101 1010 --> 11000011 10011010 --> C3 9A
+
+	fmt.Println()
 
 	fmt.Printf("Original: %s\n", "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98")
 	fmt.Printf("UTF-8: %s\n", "\xc2\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98")
